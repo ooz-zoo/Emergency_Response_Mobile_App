a67a7a987ad2be9e366d6be7b61765e0b684b5b1
JoyMicheni <111248086+ooz-zoo@users.noreply.github.com>
2024-11-07 14:50:39 +0300

    firebase setup

---
+++ lib/pages/user_auth/firebase_auth_implementation/firebase_auth_services.dart
@@ -0,0 +1,74 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple_platform_interface/authorization_credential.dart';
+import 'package:sign_in_with_apple_platform_interface/sign_in_with_apple_platform_interface.dart';
+
+class FirebaseAuthService {
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  // Method to send a password reset email
+  Future<void> sendPasswordResetEmail(String email) async {
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  //google sign-in
+  Future<User?> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        // The user canceled the sign-in
+        return null;
+      }
+
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final UserCredential userCredential = await _auth.signInWithCredential(credential);
+      return userCredential.user;
+    } catch (e) {
+      print("Error during Google sign-in: $e");
+      return null;
+    }
+  }
+
+//Sign Up
+  Future<User?> signUpWithEmailAndPassword(String email, String password) async {
+    
+    try {
+      UserCredential credential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      return credential.user;
+    }
+    catch (e) {
+      print("firebase auth error occured in signup: $e");
+    }
+
+    return null;
+  }
+
+// login
+  Future<User?> signInWithEmailAndPassword(String email, String password) async {
+
+    try {
+      UserCredential credential = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return credential.user;
+    }
+    catch (e) {
+      print("firebase auth error occured in login: $e");
+    }
+
+    return null;
+  }
+
+}
+
+
+
