8a5d27ea4d5c2a7fd261e86a43b4b3ae88c1f260
OnsongoMabeya <onsongo.developer@gmail.com>
2024-11-14 23:05:08 +0300

    Addition of 2 dart files

---
+++ e_logs.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Form',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LogScreen(),
+    );
+  }
+}
+
+class LogScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Log Screen'),
+      ),
+      body: Center(
+        child: Text(
+          'Log Screen',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
